73bd03cecdfd3c87748e485800bdabec07fd4aed
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-24 16:51:47 +0530

    commit 24/2/24

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+import logo from "./assets/logo.png"
+
 
 const Header = () => {
-  return (
-    <div>
-      Header
-    </div>
-  )
+    return (
+        <>
+        <NavLink to="/">
+            <img src="./public/logo.png" alt="" />
+        </NavLink>
+        </>
+    )
 }
 
 export default Header
