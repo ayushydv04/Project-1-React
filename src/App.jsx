7ab4664c0d579b983a329dd1d45fe0c32f461f41
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-24 16:36:12 +0530

    commit 24/2/24

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Services from "./pages/Services"
 import Contact from "./pages/Contact"
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 
 
 const App = () => {
   return (
     <>
-      <BrowserRoutes>
+      <BrowserRouter>
+      <Header />
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/about" element={<About />}/>
-          <Route path="/services" element={<Services />}/>
-          <Route path="/contact" element={<Contact />}/>
-
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
-      </BrowserRoutes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
